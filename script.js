c99862d7fb509fc9000e6456ec54e091819dc5d1
OLGA <veehja88@gmail.com>
2023-09-12 03:22:01 +0300

    правка

--- script.js
+++ script.js
@@ -1,63 +1,63 @@
-"use strict"
+// "use strict"
 
-alert("Hello, frontenders!")
+// alert("Hello, frontenders!")
 
-function multiply(a, b) {
-    console.log(a * b)
-}
-multiply(5, 6)
+// function multiply(a, b) {
+//     console.log(a * b)
+// }
+// multiply(5, 6)
 
-function sayHi(name, surname, age) {
-    console.log(`Привет, ${name}  ${surname} с возрастом ${age} лет`)
-}
-sayHi(`Иван`, `Петров`, 17)
+// function sayHi(name, surname, age) {
+//     console.log(`Привет, ${name}  ${surname} с возрастом ${age} лет`)
+// }
+// sayHi(`Иван`, `Петров`, 17)
 
-function gender(gender) {
-    if (gender == `M`) console.log(`Ваш пол мужской`)
-    else if (gender == `F`) console.log(`Ваш пол женский`)
-    else console.log(`Ваш пол неопределен`)
-}
-gender(`y`)
+// function gender(gender) {
+//     if (gender == `M`) console.log(`Ваш пол мужской`)
+//     else if (gender == `F`) console.log(`Ваш пол женский`)
+//     else console.log(`Ваш пол неопределен`)
+// }
+// gender(`y`)
 
-function sayDay(prop) {
-    let day = {
-        1: `Понедельник`,
-        2: `Вторник`,
-        3: `Среда`,
-        4: `Четверг`,
-        5: `Пятница`,
-        6: `Суббота`,
-        7: `Воскресенье`
-    };
-    console.log(day[prop])
-}
-sayDay(3)
+// function sayDay(prop) {
+//     let day = {
+//         1: `Понедельник`,
+//         2: `Вторник`,
+//         3: `Среда`,
+//         4: `Четверг`,
+//         5: `Пятница`,
+//         6: `Суббота`,
+//         7: `Воскресенье`
+//     };
+//     console.log(day[prop])
+// }
+// sayDay(3)
 
-function giveFirstWord() {
-    let str1 = "У лукоморья дуб зеленый. Златая цепь на дубе том. Днем и ночью кот ученый ходит."
-    let str2 = (str1.split("."))
-    let str3 = (str2[0].split(" ", 1) + str2[1].split(" ", 2) + str2[2].split(" ", 2))
-    console.log(str3)
-}
-giveFirstWord()
+// function giveFirstWord() {
+//     let str1 = "У лукоморья дуб зеленый. Златая цепь на дубе том. Днем и ночью кот ученый ходит."
+//     let str2 = (str1.split("."))
+//     let str3 = (str2[0].split(" ", 1) + str2[1].split(" ", 2) + str2[2].split(" ", 2))
+//     console.log(str3)
+// }
+// giveFirstWord()
 
-function sayHallo(name, hour) {
-    if (hour >= 6 && hour <= 10) { console.log(`Доброе утро, ${name}`) }
-    else if (hour >= 11 && hour <= 16) { console.log(`Добрый день, ${name}`) }
-    else if (hour >= 17 && hour <= 21) { console.log(`Добрый вечер, ${name}`) }
-    else { console.log(`Доброй ночи, ${name}`) }
-}
+// function sayHallo(name, hour) {
+//     if (hour >= 6 && hour <= 10) { console.log(`Доброе утро, ${name}`) }
+//     else if (hour >= 11 && hour <= 16) { console.log(`Добрый день, ${name}`) }
+//     else if (hour >= 17 && hour <= 21) { console.log(`Добрый вечер, ${name}`) }
+//     else { console.log(`Доброй ночи, ${name}`) }
+// }
 
-sayHallo(`Иван`, 16)
+// sayHallo(`Иван`, 16)
 
-function logAge() {
-    for (let age = 1; age <= 100; age++) {
-        if (age <= 17) console.log(`${age} -ребенок`)
-        if (age >= 18 && age <= 30) { console.log(`${age}-молод`) }
-        if (age >= 31 && age <= 55) { console.log(`${age} -зрелый`) }
-        if (age >= 56 && age <= 100) { console.log(`${age}-старый`) }
-    }
-}
+// function logAge() {
+//     for (let age = 1; age <= 100; age++) {
+//         if (age <= 17) console.log(`${age} -ребенок`)
+//         if (age >= 18 && age <= 30) { console.log(`${age}-молод`) }
+//         if (age >= 31 && age <= 55) { console.log(`${age} -зрелый`) }
+//         if (age >= 56 && age <= 100) { console.log(`${age}-старый`) }
+//     }
+// }
 
 
 
